cc7e9c449ae6bf9a23dd7fc93a17281e7575068a
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-04-12 00:05:12 +0900

    Update cell heights async

--- OutOfBoundsTableView/TableViewController.swift
+++ OutOfBoundsTableView/TableViewController.swift
@@ -14,7 +14,6 @@ class TableViewController: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
   }
   
   // MARK: - UITableViewDataSource
@@ -23,7 +22,15 @@ class TableViewController: UITableViewController {
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 5
+    return 8
+  }
+  
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    if let height = heights[indexPath.row] {
+      return height
+    } else {
+      return 44.0
+    }
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -32,6 +39,13 @@ class TableViewController: UITableViewController {
 
     cell.textLabel?.text = indexPath.description
     
+    let delay: DispatchTime = .now() + 0.5 + (Double(arc4random() % UInt32(10)) / 10.0)
+    
+    DispatchQueue.main.asyncAfter(deadline: delay) { [weak self] _ in
+      self?.heights[indexPath.row] = CGFloat((indexPath.row + 1) * 44)
+      tableView.reloadRows(at: [indexPath], with: .none)
+    }
+
     return cell
   }
 }
