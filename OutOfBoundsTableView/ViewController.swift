1a5a5553edf50bd635dba1948644067fcef7a560
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-04-12 00:36:16 +0900

    Clearing image cache to prevent app from crashing

---
+++ OutOfBoundsTableView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OutOfBoundsTableView
+//
+//  Created by mitsuyoshi.yamazaki on 2017/04/12.
+//  Copyright © 2017年 mitsuyoshi.yamazaki. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func clearImageCache(sender: AnyObject!) {
+    SDImageCache.shared().clearMemory()
+  }
+}
