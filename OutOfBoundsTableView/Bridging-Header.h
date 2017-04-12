19a2e14dc581f0e49a53f74fc7d63583cad31a3a
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-04-12 14:02:53 +0900

    Directly install SDWebImage but nothing changed

---
+++ OutOfBoundsTableView/Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Bridging-Header.h
+//  OutOfBoundsTableView
+//
+//  Created by mitsuyoshi.yamazaki on 2017/04/12.
+//  Copyright © 2017年 mitsuyoshi.yamazaki. All rights reserved.
+//
+
+#import "SDImageCache.h"
+#import "UIImageView+WebCache.h"
